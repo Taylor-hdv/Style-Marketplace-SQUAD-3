121b1b23f240edda532fed2618efd8bb473244ea
Taylor <taylorhorta@gmail.com>
2026-08-10 21:24:12 -0300

    init: primeiro commit

---
+++ front/src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes } from 'react-router'
+
+function App() {
+  return(
+    <>
+      <p className='text-amber-600'>Hello, world!</p>
+    </>
+  )
+}
+
+export default App
