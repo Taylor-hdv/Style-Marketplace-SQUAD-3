61e0ee91d2c594359c98fd8bf35fe771da329477
AlexandreRimes <alexandrerimesb@gmail.com>
2026-08-13 20:03:39 -0300

    feature: adiciona as rotas da UserController

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,1 +1,13 @@
+import { Router } from "express"; // 🔥 Adicione esta linha aqui!
+import { UserController } from "../controllers/UserController";
+    
+const router = Router();
 
+router.post("/login", UserController.login);
+router.post("/user", UserController.createUser);
+router.get("/users",  UserController.readAllUsers);
+router.get("/user/:userId",  UserController.readUser);
+router.put("/user/:userId",  UserController.updateUser);
+router.delete("/user/:userId",  UserController.deleteUser);
+
+export default router;
