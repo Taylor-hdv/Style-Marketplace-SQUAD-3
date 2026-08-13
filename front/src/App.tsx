5ed56142ec32910b305a7de752084a727f0455e4
Lucas Mendes <lmendesqueiroz@outlook.com>
2026-08-12 23:06:36 -0300

    fix: colocando responsividade completa no header e retirando imagens do .gitignore

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,9 @@
 import { BrowserRouter, Routes } from 'react-router'
-import Footer from './components/header&footer/footer'
+import Header from './components/header&footer/Header'
 
 function App() {
   return(
-    <Footer title="Stay in Style" backgoundColor="bg-blackCustom" text="Subscribe to our newsletter and be the first to know about new arrivals,
-exclusive offers, and style tips." />
+    <Header Notifications={3} />
   )
 }
 
--- front/src/components/cardProduct/CardProduct.tsx
+++ front/src/components/cardProduct/CardProduct.tsx
@@ -1,5 +1,5 @@
 import starRating from '../../assets/cardProduct/starRating.svg'
-import Button from '../button/button'
+import Button from '../button/Button'
 interface productCardProps {
     src: string;
     alt: string;
@@ -13,23 +13,25 @@ interface productCardProps {
 }
 function CardProduct({ src, alt, title, price, quantity, tag, tagColor, rating, oldPrice }: productCardProps) {
     return (
-        <section className="flex flex-col items-center justify-start w-[20rem] h-112 bg-white shadow-[0 10px 15px -3px rgba(0, 0, 0, 0.10), 0 4px 6px -4px rgba(0, 0, 0, 0.10)] rounded-xl relative" >
+        <section className="flex flex-col items-center justify-start w-[20rem] h-112 bg-white shadow-[0_10px_15px_-3px_rgba(0,0,0,0.10),0_4px_6px_-4px_rgba(0,0,0,0.10)] rounded-xl relative" >
             <div className="w-full h-80 relative">
                 <img src={src} alt={alt} className="w-full h-80 rounded-t-xl" />
                 <span className={`absolute top-3 left-3 px-[0.65rem] py-[0.2rem] rounded-[625rem] text-whiteCustom text-[0.75rem] font-semibold font-segoe ${tagColor}`}>{tag}</span>
             </div>
-            <div className="flex flex-col items-start justify-center gap-3 px-4 py-4 w-fit">
+            <div className="flex flex-col items-start justify-center gap-3 px-4 py-4 w-full">
                 <h3 className="text-blackCustom text-[1.125rem] font-semibold font-segoe">{title}</h3>
                 {rating && <div className="flex flex-row items-center justify-start gap-1">
                     <img src={starRating} alt="Star Rating" className="w-[0.8rem] h-[0.8rem]" />
                     <p className="text-blackCustom text-[0.875rem] font-segoe font-semibold">{rating}</p>
-                    <p className="text-grayCustom text-[0.875rem] font-segoe">({quantity})</p>
+                    <p className="text-[#6B7280] text-[0.875rem] font-segoe">({quantity})</p>
                 </div>}
                 <div className="flex flex-row items-center justify-start gap-2">
                     <p className="text-blackCustom text-[1.125rem] font-bold font-segoe">{price}</p>
-                    {oldPrice && <p className="text-grayCustom text-[0.875rem] font-segoe line-through">{oldPrice}</p>}
+                    {oldPrice && <p className="text-[#6B7280] text-[0.875rem] font-segoe line-through">{oldPrice}</p>}
                 </div>
-                <Button py="py-[0.78rem]" px="px-8" width="w-full" backgroundColor="bg-blackCustom" text="Add to Cart" textColor="text-blackCustom" textSize="text-[0.875rem] hasBorder="true" />
+                <div className="w-fit absolute bottom-4 right-4">
+                <Button py="py-[0.47rem]" px="px-[0.81rem]" width="w-full" backgroundColor="whiteCustom" text="Add to Cart" textSize="text-[0.875rem]" hasBorder={true} />
+                </div>
             </div>
 
         </section>
