bd5ec83de73a00938055a37b67d45915f2ec5605
AlexandreRimes <alexandrerimesb@gmail.com>
2026-08-13 22:18:26 -0300

    refactor: remove comentario desnecessario

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -1,12 +1,12 @@
-import { Router } from "express"; // 🔥 Adicione esta linha aqui!
+import { Router } from "express";
 import { UserController } from "../controllers/UserController";
     
 const router = Router();
 
 router.post("/login", UserController.login);
 router.post("/user", UserController.createUser);
-router.get("/users",  UserController.readAllUsers);
 router.get("/user/:userId",  UserController.readUser);
+router.get("/users",  UserController.readAllUsers);
 router.put("/user/:userId",  UserController.updateUser);
 router.delete("/user/:userId",  UserController.deleteUser);
 
