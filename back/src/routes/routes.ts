a0b6a1a85deb00b07fb7f14725576656b8444949
Vitor Luis <vitorluis1205@hotmail.com>
2026-08-18 21:25:21 -0300

    feature: adicionadas as rotas para as controllers de cart e order

--- back/src/routes/routes.ts
+++ back/src/routes/routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { UserController } from "../controllers/UserController";
 import { ProductController } from "../controllers/productController";
 import { VariantController } from "../controllers/variantController";
+import { CartController } from "../controllers/CartController";
+import { OrderController } from "../controllers/OrderController";
 
 const router = Router();
 
@@ -23,5 +25,17 @@ router.get("/variant/:variantId", VariantController.readVariant);
 router.get("/variants", VariantController.readAllVariants);
 router.put("/variant/:variantId", VariantController.updateVariant);
 router.delete("/variant/:variantId", VariantController.deleteVariant);
+
+router.post("/cart", CartController.createCart);
+router.get("/cart/:cartId", CartController.readCart);
+router.get("/carts", CartController.readAllCarts);
+router.put("/cart/:cartId", CartController.updateCart);
+router.delete("/cart/:cartId", CartController.deleteCart);
+
+router.post("/order", OrderController.createOrder);
+router.get("/order/:orderId", OrderController.readOrder);
+router.get("/orders", OrderController.readAllOrders);
+router.put("/order/:orderId", OrderController.updateOrder);
+router.delete("/order/:orderId", OrderController.deleteOrder);
 
 export default router;
