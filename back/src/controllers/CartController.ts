6e8f03848bc4748c3b6e45ea93555ce89d07e28a
Vitor Luis <vitorluis1205@hotmail.com>
2026-08-25 22:59:12 -0300

    feature: criada a função de adicionar variants ao carrinho

--- back/src/controllers/CartController.ts
+++ back/src/controllers/CartController.ts
@@ -112,4 +112,65 @@ export class CartController {
             return resp.status(500).json({ message: error.message });
         }
     }
+
+    public static async addVariantToCart(req: Request, resp: Response) {
+        try {
+            const { cartId } = req.params;
+            const { variantId, itemQuantity } = req.body;
+            const variant = await prisma.variant.findUnique({
+                where: { id: String(variantId) }
+            });
+            const existingCartItem = await prisma.cartVariant.findFirst({
+                where: {
+                    cartId: String(cartId),
+                    variantId: String(variantId)
+                }
+            });
+
+            if (existingCartItem) {
+                await prisma.cartVariant.update({
+                    where: { id: existingCartItem.id },
+                    data: { itemQuantity: existingCartItem.itemQuantity + itemQuantity }
+                });
+            } else {
+                const createData = {
+                    itemQuantity: itemQuantity,
+                    cart: { connect: { id: String(cartId) } },
+                    variant: { connect: { id: String(variantId) } }
+                };
+                
+                await prisma.cartVariant.create({ data: createData });
+            }
+
+            const updatedCart = await prisma.cart.findUnique({
+                where: { id: String(cartId) },
+                include: {
+                    items: {
+                        include: { variant: true }
+                    }
+                }
+            });
+
+            if (updatedCart) {
+                let newTotalQuantity = 0;
+                let newTotalPrice = 0;
+
+                updatedCart.items.forEach((item: any) => {
+                    newTotalQuantity += item.itemQuantity;
+                    newTotalPrice += (item.itemQuantity * item.variant.price);
+                });
+
+                await prisma.cart.update({
+                    where: { id: String(cartId) },
+                    data: {
+                        totalQuantity: newTotalQuantity,
+                        totalPrice: newTotalPrice
+                    }
+                });
+            }
+            return resp.status(200).json({ message: "Variante adicionada ao carrinho" });
+        } catch (error: any) {
+            return resp.status(500).json({ message: error.message });
+        }
+    }
 }
